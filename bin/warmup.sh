a4eca34d925e73f06a46f256b800af571cc6cf91
Barnacle Nightshade <58736240+humble-barnacle001@users.noreply.github.com>
2022-04-20 08:43:10 +0000

    fix: use postfix decrement operator

--- bin/warmup.sh
+++ bin/warmup.sh
@@ -5,7 +5,7 @@ until $(curl -o /dev/null -s -I -f http://localhost:$PORT); do
   sleep 5
 done
 
-while (( --count > 0 )); do
+while (( count-- > 0 )); do
   for ROUTE in $WARMUP_ROUTES; do
     echo "[warmup] calling $ROUTE, Counting down: $count"
     curl -L "http://localhost:$PORT$ROUTE" >/dev/null 2>&1
