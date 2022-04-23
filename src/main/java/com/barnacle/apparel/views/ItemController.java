d50f4c9f06f5f87a670108522e39bfa906a512d8
Barnacle Nightshade <58736240+humble-barnacle001@users.noreply.github.com>
2022-04-23 09:27:16 +0000

    feat: add buy page

--- src/main/java/com/barnacle/apparel/views/ItemController.java
+++ src/main/java/com/barnacle/apparel/views/ItemController.java
@@ -1,11 +1,15 @@
 package com.barnacle.apparel.views;
 
+import java.util.Optional;
+
+import com.barnacle.apparel.models.Item;
 import com.barnacle.apparel.models.ItemRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ItemController {
@@ -29,5 +33,14 @@ public class ItemController {
     public String getArrivalsPage(Model model) {
         model.addAttribute("arrivals", itemRepository.findNewItems());
         return "arrivals";
+    }
+
+    @GetMapping("/buy/{itemId}")
+    public String getUserDetailsById(@PathVariable("itemId") String id, Model model) {
+        // TODO: ADD BUY HABITS TO USER DB
+        Optional<Item> item = itemRepository.findById(id);
+        model.addAttribute("item", item);
+        model.addAttribute("onSoldPage", true);
+        return "itemsold";
     }
 }
