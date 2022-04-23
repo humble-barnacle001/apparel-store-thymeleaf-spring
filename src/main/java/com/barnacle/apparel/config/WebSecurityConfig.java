36d6e2980812460d66754a408a1f5faa0b3e2f35
Barnacle Nightshade <58736240+humble-barnacle001@users.noreply.github.com>
2022-04-23 09:24:23 +0000

    feat: add InMemoryUser for test

--- src/main/java/com/barnacle/apparel/config/WebSecurityConfig.java
+++ src/main/java/com/barnacle/apparel/config/WebSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.barnacle.apparel.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -12,16 +17,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/search", "/orders").authenticated()
+                .antMatchers("/search", "/orders", "/buy/**").authenticated()
                 .antMatchers("/dashboard").hasRole("ADMIN")
-                .antMatchers("/auth", "/login", "/register").not().authenticated()
                 .anyRequest().permitAll()
                 .and()
-                .formLogin()
-                .loginPage("/auth")
-                .permitAll()
+                .formLogin().loginPage("/auth").permitAll()
                 .and()
-                .logout()
-                .permitAll();
+                .logout().permitAll()
+                .and()
+                .headers().httpStrictTransportSecurity()
+                .includeSubDomains(true).maxAgeInSeconds(24 * 3600);
+    }
+
+    // TODO: USE THE USER DETAILS FROM DB
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("test@tester.co")
+                .password("6e2TF9f2kiGQR8v")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
--- src/main/java/com/barnacle/apparel/views/AuthController.java
+++ src/main/java/com/barnacle/apparel/views/AuthController.java
@@ -1,15 +1,20 @@
 package com.barnacle.apparel.views;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/auth")
 public class AuthController {
 
-    @GetMapping
+    @GetMapping("/auth")
     public String getAuthPage() {
-        return "auth";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken)
+            return "auth";
+
+        return "redirect:/";
     }
 }
