a925785f337188ba34dd27db8c5f420c551305ed
Barnacle Nightshade <58736240+humble-barnacle001@users.noreply.github.com>
2022-04-24 19:54:55 +0000

    fix: refactor code

--- src/main/java/com/barnacle/apparel/models/OrderItem.java
+++ src/main/java/com/barnacle/apparel/models/OrderItem.java
@@ -1,53 +1,7 @@
 package com.barnacle.apparel.models;
 
-import java.util.Date;
-
-import org.springframework.data.annotation.Id;
-
-public class OrderItem {
-    @Id
-    private String id;
-    private String buyerId;
-    private String itemId;
-    private Date orderOn;
+public class OrderItem extends Order {
     private Item item;
-    private float cost;
-
-    public String getId() {
-        return id;
-    }
-
-    public OrderItem setId(String id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getBuyerId() {
-        return buyerId;
-    }
-
-    public OrderItem setBuyerId(String buyerId) {
-        this.buyerId = buyerId;
-        return this;
-    }
-
-    public String getItemId() {
-        return itemId;
-    }
-
-    public OrderItem setItemId(String itemId) {
-        this.itemId = itemId;
-        return this;
-    }
-
-    public Date getOrderOn() {
-        return orderOn;
-    }
-
-    public OrderItem setOrderOn(Date orderOn) {
-        this.orderOn = orderOn;
-        return this;
-    }
 
     public Item getItem() {
         return item;
@@ -55,15 +9,6 @@ public class OrderItem {
 
     public OrderItem setItem(Item item) {
         this.item = item;
-        return this;
-    }
-
-    public Float getCost() {
-        return cost;
-    }
-
-    public OrderItem setCost(Float cost) {
-        this.cost = cost;
         return this;
     }
 }
