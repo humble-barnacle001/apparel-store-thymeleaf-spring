309fe07c47a8a34555d9a57214fff73a4f0c9a7d
Barnacle Nightshade <58736240+humble-barnacle001@users.noreply.github.com>
2022-04-25 04:12:24 +0000

    fix: avoid creation of new db Item

--- src/main/java/com/barnacle/apparel/models/OrderItem.java
+++ src/main/java/com/barnacle/apparel/models/OrderItem.java
@@ -1,7 +1,62 @@
 package com.barnacle.apparel.models;
 
-public class OrderItem extends Order {
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+
+public class OrderItem {
+    @Id
+    private String id;
+    private String buyerId;
+    private String itemId;
+    private Date orderOn;
+    private float cost;
     private Item item;
+
+    public String getId() {
+        return id;
+    }
+
+    public OrderItem setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getBuyerId() {
+        return buyerId;
+    }
+
+    public OrderItem setBuyerId(String buyerId) {
+        this.buyerId = buyerId;
+        return this;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public OrderItem setItemId(String itemId) {
+        this.itemId = itemId;
+        return this;
+    }
+
+    public Date getOrderOn() {
+        return orderOn;
+    }
+
+    public OrderItem setOrderOn(Date orderOn) {
+        this.orderOn = orderOn;
+        return this;
+    }
+
+    public Float getCost() {
+        return cost;
+    }
+
+    public OrderItem setCost(Float cost) {
+        this.cost = cost;
+        return this;
+    }
 
     public Item getItem() {
         return item;
